40f956e9463da7601cc4667d5194e4a31e6980f6
Liam <hillierliam37@gmail.com>
2023-12-05 17:45:16 +1100

    layout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
     <header className="fixed top-0 top-[32px]">
       <Disclosure as="nav" className="bg-gray-800 w-full">
         {({ open }) => (
-          <div className=" mx-auto px-4 sm:px-6 lg:px-8 flex h-16 justify-between max-w-6xl">
+          <div className=" mx-auto px-4 sm:px-6 lg:px-20 flex h-16 justify-between ">
             <div className="flex">
               <div className="-ml-2 mr-2 flex items-center md:hidden">
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <div className="relative">
       <Header />
-      <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pb-20 mt-10">
+      <main className="mx-auto px-4 sm:px-6 lg:px-20 pb-20 mt-10">
         {children}
       </main>
     </div>
