2eac5d4dbd542ac8c9018c32be028988d73c65cd
LiamHillier <hillierliam37@gmail.com>
2024-01-02 19:40:40 +1100

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -43,18 +43,6 @@ export default function Header() {
               </div>
               <Main navItems={navigation} />
             </div>
-            <div className="flex items-center">
-              <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
           </div>
         )}
       </Disclosure>
